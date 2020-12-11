c87c7826f4b1c64cf089c7ef60b236829d103c78
Oktay GÜLEÇ <oktay.gulec@outlook.com>
2020-12-11 23:00:28 +0300

    Update Program.cs

--- TrainStation/Program.cs
+++ TrainStation/Program.cs
@@ -11,8 +11,8 @@ namespace TrainStation
         {
             while (true)
             {
-                //Get the total location number and total path number from user.
                 //Kullanıcıdan toplam konum ve yol sayısını al.
+                //Get the total location number and total path number from the user.
                 Console.Write("Enter the total location number: ");
                 TotalLocation = Convert.ToInt32(Console.ReadLine());
                 Console.Write("Enter the total path number: ");
@@ -25,7 +25,7 @@ namespace TrainStation
         }
 
         //Örnek tren istasyonu yarat.
-        //Make a sample train station.
+        //Create a sample train station.
         static void CreateStation(int tp, int tl)
         {
             Paths = new char[tp, tl];
@@ -34,14 +34,16 @@ namespace TrainStation
             {
                 for (int j = 0; j < tl; j++)
                 {
-                    /*Make a random number between 0 and 2 (it's going to be 0 or 1) 
-                    convert them to * or - (* is empty and - is unlit location)
-                    and put them into a 2D Array which is a train station
+                    /*
+                    0 ile 2 arasında bir rastgele sayı yarat (bu sayı 0 veya 1 olacak)
+                    bu sayıları * ya da - olarak dönüştür (* : boş ve - : lamba koyulmamış konum)
+                    ve bu değerleri iki boyutlu bir dizi içerisine koy (bu dizi tren istasyonu olacaktır).
                     */
-
-                    /*0 ile 2 arasında bir rastgele sayı yarat (bu sayı 0 veya 1 olacak)
-                     bu sayıları * ya da - olarak dönüştür (* : boş ve - : lamba koyulmamış konum)
-                     ve bu değerleri iki boyutlu bir dizi içerisine koy (bu dizi tren istasyonu olacaktır).
+                    
+                    /*
+                    Create a random number between 0 and 2 (it is going to be 0 or 1) 
+                    convert them to * or - (* is empty and - is unlit location)
+                    and put them into a 2D Array which is a train station.
                     */
                     Random RN = new Random();
                     int RandomNumber = RN.Next(0, 2);
@@ -62,7 +64,7 @@ namespace TrainStation
         }
 
         //Bulunduğum konum uygunsa, istasyonlara lamba koy.
-        //Put lights at the stations if it's in correct place.
+        //Put lights at the stations if it is in the correct location.
         static void PutLight(char[,] path, int tp, int tl)
         {
             int totalStation = 0;
@@ -72,29 +74,29 @@ namespace TrainStation
                 for (int j = 0; j < tl; j++)
                 {
                     //Konumum istasyonsa ve daha önce istasyona uğramadıysam lamba koy.
-                    //If my current location is a station and if I didn't stop by any station before, then put a light.
+                    //If my current location is a station and if I did not stop by any station before, then put a light.
                     if (path[i, j] == '-' && totalStation == 0)
                     {
                         path[i, j] = '+';
                         totalStation++;
                     }
                     //Konumum istasyonsa ve daha önce istasyona uğradıysam devam et.
-                    //If my current location is a station and if I did stop by any station before, then continue.
+                    //If my current location is a station and if I did stop by a station before, then continue.
 
                     else if (path[i, j] == '-' && totalStation > 0)
                     {
                         totalStation++;
                     }
                     //Konumum istasyon değilse ve daha önce istasyona uğradıysam bir önceki konuma lamba koy.
-                    //If my current location is not a station and if I did stop by any station before, then put a light before the current location.
+                    //If my current location is not a station and if I did stop by a station before, then put a light at the previous location.
 
                     else if (path[i, j] == '*' && totalStation > 0)
                     {
                         path[i, j - 1] = '+';
                         totalStation = 0;
                     }
-                    //Konumum istasyonsa ve yolun sonundaysam lamba koy
-                    //If my current location is a station and if I'm at the end of the path, then put a light.
+                    //Konumum istasyonsa ve yolun sonundaysam lamba koy.
+                    //If my current location is a station and if I am at the end of the path, then put a light.
                     if (path[i, j] == '-' && j == tl - 1)
                     {
                         path[i, j] = '+';
@@ -105,8 +107,8 @@ namespace TrainStation
             }
         }
 
-        //Verilen tren istasyonunu düzeltip ekranda göster.
-        //Fix the train station where given to the program and show on the display.
+        //Verilen tren istasyonunu düzelt ve ekranda göster.
+        //Fix the given train station and show on the screen.
         static void CreateFixedStation(char[,] path, int tp, int tl)
         {
 
